328c89e16d3b3da18f536497253ed71d48fd5951
mobilevisuals <hwddheedj63!>
2020-03-13 14:36:43 +0100

    1

---
+++ src/ViewActionManagedBean.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
+
+/**
+ *
+ * @author Admin
+ */
+@Named(value = "viewActionManagedBean")
+@SessionScoped
+public class ViewActionManagedBean implements Serializable {
+
+    /**
+     * Creates a new instance of ViewActionManagedBean
+     */
+    public ViewActionManagedBean() {
+    }
+
+    public String turn()
+    {
+        int i=0;
+        i++;
+        return "page3";}
+
+}
